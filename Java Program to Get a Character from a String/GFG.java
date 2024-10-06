6e1c11053926091d2c46815f4baa527210251ad0
vrushalip049 <45143287+vrushalip049@users.noreply.github.com>
2024-10-06 15:28:16 +0530

    This is 2nd release

--- Java Program to Get a Character from a String/GFG.java
+++ Java Program to Get a Character from a String/GFG.java
@@ -1,39 +1,35 @@
 // Java program to get a specific character
 // from a given String at a specific index
 
-class GFG{
+class GFG {
 
-    // Driver code
-    public static void main(String[] args)
-    {
+	// Driver code
+	public static void main(String[] args) {
 
-       String str1="Welcome World ";
-       String str2="India";
-       System.out.println("Before------------");
-       System.out.println(str1);
-       System.out.println(str2);
-       
-       String str3=str1.concat(str2);
-       System.out.println("After-------------");
-       System.out.println(str1);
-       System.out.println(str2);
-       System.out.println(str3);
-        
-       char ch=str3.charAt(6);
-       String str4="";
-       System.out.println("Char at 6->"+ch);
-       for (int i=str3.length();i>=0;i--)
-       {
-    	   str4=str4 +charAt(i);
-    	   System.out.println(str4);
-       }
-       
-       
-    }
+		String str1 = "Welcome World ";
+		String str2 = "India";
+		System.out.println("Before------------");
+		System.out.println(str1);
+		System.out.println(str2);
+
+		String str3 = str1.concat(str2);
+		System.out.println("After-------------");
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+
+		char ch = str3.charAt(6);
+		String str4 = "";
+		System.out.println("Char at 6->" + ch);
+		for (int i = str3.length(); i >= 0; i--) {
+			str4 = str4 + charAt(i);
+			System.out.println(str4);
+		}
+
+	}
 
 	private static String charAt(int i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 }
-
